23bf24472df2af0f065e34091a563506654ef8c2
Vidhya0101 <vidhya7117@gmail.com>
2025-05-06 09:25:54 +0530

    Create reverse.py

---
+++ 04.05.2025/reverse.py
@@ -0,0 +1,14 @@
+def reverse_number(number):
+    original_number = number
+    reverse = 0
+
+    for _ in str(number):
+        digit = number % 10
+        reverse = reverse * 10 + digit
+        number //= 10
+
+    print(f&quot;Original Number: {original_number}&quot;)
+    print(f&quot;Reversed Number: {reverse}&quot;)
+
+reverse = 6789
+reverse_number(reverse)
